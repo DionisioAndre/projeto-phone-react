c99ee6bd7381649a34a2bc44c94600eaa2083541
Dionisio Andre <you@example.com>
2024-10-29 18:48:24 +0000

    Sua7

--- src/views/admin/OrderList/index.jsx
+++ src/views/admin/OrderList/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
 import AuthContext from '../../../context/authContext';
 import { useNavigate } from "react-router-dom";
-import { Modal, Button,Container } from 'react-bootstrap';
+import { Modal, Button, Container } from 'react-bootstrap';
 
 import Loading from '../../../components/Loading';
 import { logoutUser } from '../../../services/localStorege';
@@ -10,7 +10,7 @@ import './OrderList.css';
 
 const OrderList = () => {
     const { user } = useContext(AuthContext);
-const navigate = useNavigate();
+    const navigate = useNavigate();
     const [orders, setorders] = useState([]);
     const [error, setError] = useState('');
     const [showModal, setShowModal] = useState(false);
@@ -36,14 +36,13 @@ const navigate = useNavigate();
                 setorders(response.data);
             } catch (err) {
                 setError('Erro ao buscar produtos.');
-                if(err.response.status==401){
-                    alert("A sessao expirou! por favor faça o iogin")
-                    logoutUser
+                if (err.response.status === 401) {
+                    alert("A sessao expirou! por favor faça o iogin");
+                    logoutUser(); // Corrigido para chamar a função
                     navigate('/loginpage');
-                } else{
+                } else {
                     alert(error);
                 }
-               
             } finally {
                 setLoading(false);
             }
@@ -65,14 +64,13 @@ const navigate = useNavigate();
                 setorders(orders.filter(order => order.id !== orderId));
             } catch (err) {
                 setError('Erro ao apagar produto.');
-                if(err.response.status==401){
-                    alert("A sessao expirou! por favor faça o iogin")
-                    logoutUser
+                if (err.response.status === 401) {
+                    alert("A sessao expirou! por favor faça o iogin");
+                    logoutUser(); // Corrigido para chamar a função
                     navigate('/loginpage');
-                } else{
+                } else {
                     alert(error);
                 }
-               
             }
         }
     };
@@ -112,14 +110,13 @@ const navigate = useNavigate();
             await fetchorders();
         } catch (err) {
             setError('Erro ao atualizar produto.');
-            if(err.response.status==401){
-                alert("A sessao expirou! por favor faça o iogin")
-                logoutUser
+            if (err.response.status === 401) {
+                alert("A sessao expirou! por favor faça o iogin");
+                logoutUser(); // Corrigido para chamar a função
                 navigate('/loginpage');
-            } else{
+            } else {
                 alert(error);
             }
-           
         }
     };
 
@@ -140,18 +137,16 @@ const navigate = useNavigate();
                     Authorization: `Bearer ${user.token}`
                 }
             });
-            
             setorders(response.data);
         } catch (err) {
             setError('Erro ao buscar produtos.');
-            if(err.response.status==401){
-                alert("A sessao expirou! por favor faça o iogin")
-                logoutUser
+            if (err.response.status === 401) {
+                alert("A sessao expirou! por favor faça o iogin");
+                logoutUser(); // Corrigido para chamar a função
                 navigate('/loginpage');
-            } else{
+            } else {
                 alert(error);
             }
-           
         }
     };
 
@@ -161,87 +156,87 @@ const navigate = useNavigate();
 
     return (
         <div className="custom-background">
-        <Container className="mt-5">
-        <div>
-            <h2>Lista de Produtos</h2>
-            {error && <p className="text-danger">{error}</p>}
-            <div className="row">
-          {orders.map(order => (
-            <div key={order.id}className="col-md-4 mb-4">
-              <h3>Pedido ID: {order.id}</h3>
-              <h3>Pedido Buyer: {order.buyer}</h3>
-              <img src={formatImageUrl(order.image)} alt="imagem"className="card-img-top cor" />
-              <p className="card-text">produto:{order.product}</p>
-              <p className="card-text">Preço: KZ{order.price}</p>
-              <p>Quantidade: {order.quantity}</p>
-              <p>Data do Pedido: {new Date(order.created_at).toLocaleDateString()}</p>
-              <Button variant="danger" onClick={() => handleDelete(order.id)}>Apagar</Button>
-              <hr />
-            </div>
-          ))}
-          </div>
+            <Container className="mt-5">
+                <div>
+                    <h2>Lista de Produtos</h2>
+                    {error && <p className="text-danger">{error}</p>}
+                    <div className="row">
+                        {orders.map(order => (
+                            <div key={order.id} className="col-md-4 mb-4">
+                                <h3>Pedido ID: {order.id}</h3>
+                                <h3>Pedido Buyer: {order.buyer}</h3>
+                                <img src={formatImageUrl(order.image)} alt="imagem" className="card-img-top cor" />
+                                <p className="card-text">produto: {order.product}</p>
+                                <p className="card-text">Preço: KZ{order.price}</p>
+                                <p>Quantidade: {order.quantity}</p>
+                                <p>Data do Pedido: {new Date(order.created_at).toLocaleDateString()}</p>
+                                <Button variant="danger" onClick={() => handleDelete(order.id)}>Apagar</Button>
+                                <hr />
+                            </div>
+                        ))}
+                    </div>
 
-            {/* Modal para atualizar produto */}
-            <Modal show={showModal} onHide={() => setShowModal(false)}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Atualizar Produto</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label className="form-label">Nome</label>
-                            <input 
-                                type="text" 
-                                className="form-control" 
-                                value={name} 
-                                onChange={(e) => setName(e.target.value)} 
-                                required 
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Tipo</label>
-                            <input 
-                                type="text" 
-                                className="form-control" 
-                                value={tipo} 
-                                onChange={(e) => setTipo(e.target.value)} 
-                                required 
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Preço</label>
-                            <input 
-                                type="number" 
-                                className="form-control" 
-                                value={price} 
-                                onChange={(e) => setPrice(e.target.value)} 
-                                required 
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Descrição</label>
-                            <textarea 
-                                className="form-control" 
-                                value={description} 
-                                onChange={(e) => setDescription(e.target.value)} 
-                                required 
-                            />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Escolha uma nova imagem (opcional)</label>
-                            <input 
-                                type="file" 
-                                className="form-control" 
-                                onChange={handleImageChange} 
-                            />
-                        </div>
-                        <Button variant="primary" type="submit">Atualizar</Button>
-                    </form>
-                </Modal.Body>
-            </Modal>
+                    {/* Modal para atualizar produto */}
+                    <Modal show={showModal} onHide={() => setShowModal(false)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Atualizar Produto</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label className="form-label">Nome</label>
+                                    <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        value={name} 
+                                        onChange={(e) => setName(e.target.value)} 
+                                        required 
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Tipo</label>
+                                    <input 
+                                        type="text" 
+                                        className="form-control" 
+                                        value={tipo} 
+                                        onChange={(e) => setTipo(e.target.value)} 
+                                        required 
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Preço</label>
+                                    <input 
+                                        type="number" 
+                                        className="form-control" 
+                                        value={price} 
+                                        onChange={(e) => setPrice(e.target.value)} 
+                                        required 
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Descrição</label>
+                                    <textarea 
+                                        className="form-control" 
+                                        value={description} 
+                                        onChange={(e) => setDescription(e.target.value)} 
+                                        required 
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Escolha uma nova imagem (opcional)</label>
+                                    <input 
+                                        type="file" 
+                                        className="form-control" 
+                                        onChange={handleImageChange} 
+                                    />
+                                </div>
+                                <Button variant="primary" type="submit">Atualizar</Button>
+                            </form>
+                        </Modal.Body>
+                    </Modal>
+                </div>
+            </Container>
         </div>
-        </Container>
-    </div>
     );
 };
 
